f5ff0a818b8e85f499af490f8de3372ac04bc2f5
Hantamico <89966706+Hantamico@users.noreply.github.com>
2023-06-19 11:59:13 +0000

    Deploying to gh-pages from @ Hantamico/goit-react-hw-05-movies@1302270a80e493721ff99d79caf2dcdb7ea8437b 🚀

---
+++ static/js/805.164cebc8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsx)("div",{style:{height:"100vh",display:"flex",justifyContent:"center",alignItems:"center",fontSize:40,color:"#010101"},children:"Not Found"})}}}]);
+//# sourceMappingURL=805.164cebc8.chunk.js.map
